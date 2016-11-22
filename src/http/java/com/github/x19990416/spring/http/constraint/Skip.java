4a3c49e7f6ac4b1a08a152d379bd1c8f4f0ac490
x19990416 <starseeker.limin@gmail.com>
2016-11-22 10:45:23 +0800

    add

---
+++ src/http/java/com/github/x19990416/spring/http/constraint/Skip.java
@@ -0,0 +1,17 @@
+package com.github.x19990416.spring.http.constraint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE,ElementType.METHOD})
+/**
+ * 跳过{@code SessionCheck}
+ * @author starseeker.limin@gmail.com(X-Forever)
+ *
+ */
+public @interface Skip {
+
+}
