aba81734148b0573940d43f9a2da97e59623d1ae
x19990416 <starseeker.limin@gmail.com>
2016-12-21 13:56:27 +0800

    update

--- src/wechat/java/com/github/x19990416/macrossx/spring/wechat/IWechatCardHelper.java
+++ src/wechat/java/com/github/x19990416/macrossx/spring/wechat/IWechatCardHelper.java
@@ -4,18 +4,21 @@ import java.io.InputStream;
 import java.util.Optional;
 
 import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardActivation;
-import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardCreation;
-import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardUpdateReqObj;
-import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardUpdateRespObj;
+import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardCreateReqObj;
+import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardCreateRespObj;
+import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardUserUpdateReqObj;
+import com.github.x19990416.macrossx.spring.wechat.entity.WechatCardUserUpdateRespObj;
 import com.github.x19990416.macrossx.spring.wechat.entity.WechatLogo;
 import com.github.x19990416.macrossx.spring.wechat.entity.WechatResponseObj;
 
 public interface IWechatCardHelper {
 	public Optional<WechatLogo> uploadLogo(InputStream inputStream);
 
-	public Optional<WechatResponseObj> create(WechatCardCreation wechatCard);
+	public Optional<WechatCardCreateRespObj> create(WechatCardCreateReqObj wechatCard);
 
 	public Optional<WechatResponseObj> activate(WechatCardActivation activation);
 
-	public Optional<WechatCardUpdateRespObj> update(WechatCardUpdateReqObj update);
+	public Optional<WechatCardUserUpdateRespObj> userUpdate(WechatCardUserUpdateReqObj update);
+	
+	public Optional<WechatCardCreateRespObj> update(WechatCardCreateReqObj wechatCard);
 }
